e4ab6eb843df80f524bedb3ab66b9dc0783fb7de
Vladimir <imac@iMac-Ekaterina.local>
2017-03-31 13:56:16 +0300

    BusinesTheme

---
+++ js/common.js
@@ -0,0 +1,14 @@
+$(function() {
+
+	$('#navigation').slimmenu(
+	{
+		resizeWidth: '768',
+		collapserTitle: 'Busines Theme',
+		animSpeed: 'medium',
+		easingEffect: null,
+		indentChildren: false,
+		childrenIndenter: '&nbsp;'
+	});
+
+});
+
